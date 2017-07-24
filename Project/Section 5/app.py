2a0c2f110b006a5e5dccf62836da6e6bcc1dd732
Dhiraj Borade <dhirajborade@gmail.com>
2017-07-24 16:58:16 -0400

    Signing up

--- Project/Section 5/app.py
+++ Project/Section 5/app.py
@@ -3,6 +3,7 @@ from flask_restful import Resource, Api, reqparse
 from flask_jwt import JWT, jwt_required
 
 from security import authenticate, identity
+from user import UserRegister
 
 app = Flask(__name__)
 app.secret_key = 'MY_SECRET_KEY'
@@ -62,6 +63,7 @@ class ItemList(Resource):
 
 api.add_resource(Item, '/item/<string:name>')
 api.add_resource(ItemList, '/items')
+api.add_resource(UserRegister, '/register')
 
 if __name__ == "__main__":
     app.run(host='127.0.0.1', port=5001, debug=True)
