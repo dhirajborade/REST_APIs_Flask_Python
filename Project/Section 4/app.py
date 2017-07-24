9dec7bbe7ca8ad99347a1a9c08fcc4e3e3c6bc53
Dhiraj Borade <dhirajborade@gmail.com>
2017-07-23 23:04:49 -0400

    Completed Authorization and Logging

--- Project/Section 4/app.py
+++ Project/Section 4/app.py
@@ -1,9 +1,14 @@
 from flask import Flask, request
 from flask_restful import Resource, Api
+from flask_jwt import JWT, jwt_required
+
+from security import authenticate, identity
 
 app = Flask(__name__)
 app.secret_key = 'MY_SECRET_KEY'
 api = Api(app)
+
+jwt = JWT(app, authenticate, identity) # /auth
 
 # class Student(Resource):
 #     def get(self, name):
@@ -14,9 +19,12 @@ api = Api(app)
 items = []
 
 class Item(Resource):
+
+    @jwt_required()
     def get(self, name):
         item = next(filter(lambda x : x['name'] == name, items), None)
         return {'item': item}, 200 if item else 404
+
     def post(self, name):
         if next(filter(lambda x : x['name'] == name, items), None) is not None:
             return {'message': "An item with name '{}' already exists.".format(name)}, 400
@@ -26,6 +34,7 @@ class Item(Resource):
         return item, 201
 
 class ItemList(Resource):
+
     def get(self):
         return {'items': items}
 
