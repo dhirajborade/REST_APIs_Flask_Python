a3bf2cf77f959376e1a755ebf600be82a97310bd
Dhiraj Borade <dhirajborade@gmail.com>
2017-07-24 18:11:52 -0400

    Auto stash before merge of "master" and "origin/master"

--- Project/Section 5/item.py
+++ Project/Section 5/item.py
@@ -4,6 +4,7 @@ from flask_restful import Resource, reqparse
 
 
 class Item(Resource):
+
     parser = reqparse.RequestParser()
     parser.add_argument('price',
                         type=float,
@@ -13,6 +14,13 @@ class Item(Resource):
 
     @jwt_required()
     def get(self, name):
+        item = self.find_by_name(name)
+        if item:
+            return item
+        return {'message': 'Item not found'}, 404
+
+    @classmethod
+    def find_by_name(cls, name):
         connection = sqlite3.connect('data.db')
         cursor = connection.cursor()
 
@@ -24,14 +32,22 @@ class Item(Resource):
         if row:
             return {'item': {'name': row[0], 'price': row[1]}}
 
-        return {'message': 'Item not found'}, 404
-
     def post(self, name):
-        if next(filter(lambda x: x['name'] == name, items), None) is not None:
+        if self.find_by_name(name):
             return {'message': "An item with name '{}' already exists.".format(name)}, 400
+        
         data = Item.parser.parse_args()
         item = {'name': name, 'price': data['price']}
-        items.append(item)
+
+        connection = sqlite3.connect('data.db')
+        cursor = connection.cursor()
+
+        query = "INSERT INTO items VALUES (?, ?)"
+        cursor.execute(query, (item['name'], item['price']))
+
+        connection.commit()
+        connection.close()
+
         return item, 201
 
     def delete(self, name):
