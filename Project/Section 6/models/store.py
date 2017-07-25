ba2465e90989342dd1369755159159332dbea0a5
Dhiraj Borade <dhirajborade@gmail.com>
2017-07-25 02:55:37 -0400

    Completed Section 6

--- Project/Section 6/models/store.py
+++ Project/Section 6/models/store.py
@@ -1,8 +1,8 @@
 from db import db
 
 
-class ItemModel(db.Model):
-    __tablename__ = 'items'
+class StoreModel(db.Model):
+    __tablename__ = 'stores'
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80))
