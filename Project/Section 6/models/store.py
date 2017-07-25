420503f827024f788c2dc9f452a9522a7062a952
Dhiraj Borade <dhirajborade@gmail.com>
2017-07-25 02:28:51 -0400

    Complted StoreModel

--- Project/Section 6/models/store.py
+++ Project/Section 6/models/store.py
@@ -6,14 +6,14 @@ class ItemModel(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80))
-    price = db.Column(db.Float(precision=2))
 
-    def __init__(self, name, price):
+    items = db.relationship('ItemModel', lazy='dynamic')
+
+    def __init__(self, name):
         self.name = name
-        self.price = price
 
     def json(self):
-        return {'name': self.name, 'price': self.price}
+        return {'name': self.name, 'items': [item.json() for item in self.items.all()]}
 
     @classmethod
     def find_by_name(cls, name):
