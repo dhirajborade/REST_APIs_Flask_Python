de0c60b472151f4965190a928c3092a40aad5dcb
Dhiraj Borade <dhirajborade@gmail.com>
2017-07-25 02:02:07 -0400

    ItemModel

--- Project/Section 6/models/user.py
+++ Project/Section 6/models/user.py
@@ -14,34 +14,18 @@ class UserModel(db.Model):
         self.username = username
         self.password = password
 
+    def save_to_db(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def delete_from_db(self):
+        db.session.delete(self)
+        db.session.commit()
+
     @classmethod
     def find_by_username(cls, username):
-        connection = sqlite3.connect('data.db')
-        cursor = connection.cursor()
-
-        query = "SELECT * FROM users WHERE username=?"
-        result = cursor.execute(query, (username,))
-        row = result.fetchone()
-        if row:
-            user = cls(*row)
-        else:
-            user = None
-
-        connection.close()
-        return user
+        return cls.query.filter_by(username=username).first()
 
     @classmethod
     def find_by_id(cls, _id):
-        connection = sqlite3.connect('data.db')
-        cursor = connection.cursor()
-
-        query = "SELECT * FROM users WHERE id=?"
-        result = cursor.execute(query, (_id,))
-        row = result.fetchone()
-        if row:
-            user = cls(*row)
-        else:
-            user = None
-
-        connection.close()
-        return user
+        return cls.query.filter_by(id=_id).first()
--- Project/Section 6/resources/item.py
+++ Project/Section 6/resources/item.py
@@ -27,52 +27,43 @@ class Item(Resource):
         item = ItemModel(name, data['price'])
 
         try:
-            item.insert_item()
+            item.save_to_db()
         except:
             return {'message': "An error occurred while inserting the item"}, 500
 
         return item.json(), 201
 
     def delete(self, name):
-        connection = sqlite3.connect('data.db')
-        cursor = connection.cursor()
-
-        query = "DELETE FROM items WHERE name=?"
-        cursor.execute(query, (name,))
-
-        connection.commit()
-        connection.close()
+        item = ItemModel.find_by_name(name)
+        if item:
+            item.delete_from_db()
 
         return {'message': 'Item Deleted'}
 
     def put(self, name):
         data = Item.parser.parse_args()
         item = ItemModel.find_by_name(name)
-        updated_item = ItemModel(name, data['price'])
         if item is None:
-            try:
-                updated_item.insert_item()
-            except:
-                return {'message': "An error occurred while inserting the item."}, 500
+            item = ItemModel(name, data['price'])
         else:
-            try:
-                updated_item.update()
-            except:
-                return {'message': "An error occurred inserting the item."}, 500
-        return updated_item.json()
+            item.price = data['price']
+
+        item.save_to_db()
+        return item.json()
 
 
 class ItemList(Resource):
     def get(self):
-        connection = sqlite3.connect('data.db')
-        cursor = connection.cursor()
-
-        query = "SELECT * FROM items"
-        result = cursor.execute(query)
-        items = []
-        for row in result:
-            items.append({'name': row[0], 'price': row[1]})
-
-        connection.close()
-
-        return {'items': items}
+        return db.query.all()
+        # connection = sqlite3.connect('data.db')
+        # cursor = connection.cursor()
+        #
+        # query = "SELECT * FROM items"
+        # result = cursor.execute(query)
+        # items = []
+        # for row in result:
+        #     items.append({'name': row[0], 'price': row[1]})
+        #
+        # connection.close()
+        #
+        # return {'items': items}
