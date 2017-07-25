df371434ce5d27f0566c92e1b5d74cdd32b0193f
Dhiraj Borade <dhirajborade@gmail.com>
2017-07-24 20:50:57 -0400

    Section 5 Completed

--- Project/Section 5/item.py
+++ Project/Section 5/item.py
@@ -4,7 +4,6 @@ from flask_restful import Resource, reqparse
 
 
 class Item(Resource):
-
     parser = reqparse.RequestParser()
     parser.add_argument('price',
                         type=float,
@@ -35,10 +34,19 @@ class Item(Resource):
     def post(self, name):
         if self.find_by_name(name):
             return {'message': "An item with name '{}' already exists.".format(name)}, 400
-        
+
         data = Item.parser.parse_args()
         item = {'name': name, 'price': data['price']}
 
+        try:
+            self.insert_item(item)
+        except:
+            return {'message': "An error occurred inserting the item"}, 500
+
+        return item, 201
+
+    @classmethod
+    def insert_item(cls, item):
         connection = sqlite3.connect('data.db')
         cursor = connection.cursor()
 
@@ -47,8 +55,6 @@ class Item(Resource):
 
         connection.commit()
         connection.close()
-
-        return item, 201
 
     def delete(self, name):
         connection = sqlite3.connect('data.db')
@@ -64,15 +70,43 @@ class Item(Resource):
 
     def put(self, name):
         data = Item.parser.parse_args()
-        item = next(filter(lambda x: x['name'] == name, items), None)
+        item = self.find_by_name(name)
+        updated_item = {'name': name, 'price': data['price']}
         if item is None:
-            item = {'name': name, 'price': data['price']}
-            items.append(item)
+            try:
+                self.insert_item(updated_item)
+            except:
+                {'message': "An error occurred inserting the item."}, 500
         else:
-            item.update(data)
-        return item
+            try:
+                self.update(updated_item)
+            except:
+                {'message': "An error occurred inserting the item."}, 500
+        return updated_item
+
+    @classmethod
+    def update(cls, item):
+        connection = sqlite3.connect('data.db')
+        cursor = connection.cursor()
+
+        query = "UPDATE items SET price=? WHERE name=?"
+        cursor.execute(query, (item['price'], item['name']))
+
+        connection.commit()
+        connection.close()
 
 
 class ItemList(Resource):
     def get(self):
+        connection = sqlite3.connect('data.db')
+        cursor = connection.cursor()
+
+        query = "SELECT * FROM items"
+        result = cursor.execute(query)
+        items = []
+        for row in result:
+            items.append({'name': row[0], 'price': row[1]})
+
+        connection.close()
+
         return {'items': items}
