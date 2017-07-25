07b694b54c1037b7d1791482032406acd33a76d0
Dhiraj Borade <dhirajborade@gmail.com>
2017-07-25 02:17:23 -0400

    ItemList

--- Project/Section 6/app.py
+++ Project/Section 6/app.py
@@ -12,6 +12,10 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'MY_SECRET_KEY'
 api = Api(app)
 
+@app.before_first_request
+def create_tables():
+    db.create_all()
+
 jwt = JWT(app, authenticate, identity)  # /auth
 
 # class Student(Resource):
